9f0dc09c6cc4fd256fb34f6fe1fbd6bd75c649a0
JGardener <jamesgardener92@gmail.com>
2020-04-04 18:43:40 +0100

    Fixed Clip functionality. Removed Discord.js,

--- src/Commands/Clip.js
+++ src/Commands/Clip.js
@@ -1,6 +1,5 @@
 import fetch from "node-fetch"
 
-
 const refreshToken = (API_CLIENT_ID, CLIENT_SECRET, REFRESH_CODE) => {
 // Refresh Access Token
 let accessToken = ""
@@ -10,8 +9,8 @@ return fetch(`https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh
   .then((response) => {  
     return response.json()})
       .then((data) => {
-     
       accessToken = `Bearer ${data["access_token"]}`;   
+     console.log("I'm inside refreshToken and I'm the accessToken" + accessToken)
       return accessToken    
 } 
   )
@@ -20,25 +19,52 @@ return fetch(`https://id.twitch.tv/oauth2/token?grant_type=refresh_token&refresh
 
 const createClip = (client, channel, userstate, accessToken, API_CLIENT_ID) => {
 // console.log("Here's your access token! " + accessToken)
-fetch(`https://api.twitch.tv/helix/clips?broadcaster_id=36866421`, {
-      method: "POST",
-      headers: {
-        'Client-ID': API_CLIENT_ID,
-        'Authorization': accessToken
+return fetch(`https://api.twitch.tv/helix/clips?broadcaster_id=36866421`, {
+  method: "POST",
+  headers: {
+    'Client-ID': API_CLIENT_ID,
+    'Authorization': accessToken
         
-        }}).then((response) => {
-          return response.json()
-        }).then((list) => {
-          console.log(list)
-          client.say(channel,`${userstate["display-name"]} Thanks for clipping! Edit your clip here! ${list.data[0]["edit_url"]} `)
-        }
-      )
+  }})
+    .then((response) => {
+      return response.json()
+    })
+      .then((list) => {
+        let editURL = list.data[0]["edit_url"];
+        let user = userstate["display-name"];
+        client.say(channel,`${user} Thanks for clipping! You'll find your clip in the Discord #clips-highlights channel.`)
+    
+          return {
+          user: userstate["display-name"],
+          editURL: list.data[0]["edit_url"]
+        }})
+  
 }
+
+const sendToDiscord = (user,editURL) => {
+console.log("This is inside the sendToDiscord" + editURL)
+return fetch("https://discordapp.com/api/webhooks/695516341527904318/q1dYd8isAcXKf6JTmt_xu0TtyLtz3NvvoJFh96P-b98QB2OAqCIin-1myEYj-BBt5_5a", {
+  method: "POST",
+  headers: {
+    'Content-Type': 'application/json'
+    },
+
+  body: JSON.stringify({
+    username: "RomBot",
+    content: `${user} has clipped! ${editURL}`
+  })
+  })
+}
+
 
 const Clip = (client, channel, userstate, API_CLIENT_ID, CLIENT_SECRET, REFRESH_CODE) => {
   refreshToken(API_CLIENT_ID, CLIENT_SECRET, REFRESH_CODE)
     .then((accessToken) => {
-    createClip(client, channel, userstate, accessToken, API_CLIENT_ID)})
+      createClip(client, channel, userstate, accessToken, API_CLIENT_ID)
+        .then((object) => {
+          console.log(object.user, object.editURL)
+          sendToDiscord(object.user, object.editURL)})})
+        
  
 }
 
--- src/Commands/Discord.js
+++
@@ -1,9 +0,0 @@
-const Discord = (client, channel) => {
-    client.say(channel, "Join the community Discord! https://discord.gg/8VyXumH")    
-}
-
-export default Discord
-
-
-// Replace the client.say in the create Clip function with the message to send to chat. 
-// Add a function that takes in the createClip return and sends it to Discord with a message attached. 
